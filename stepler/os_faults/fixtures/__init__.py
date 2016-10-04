09024bfef63cd1572b81c6da0bd2e523c9860e23
Sergey Sokolov <ssm@mera.ru>
2016-10-04 13:45:40 +0000

    Keystone tests
    - new tests - test_restart_all_services,
      test_ldap_basic_functions, test_ldap_get_group_members
    - new file test_keystome_ldap.py
    - new fixtures and steps for groups
    - new steps for domains
    - taking into account domains and groups in fixtures and steps for users
    - new fixtures tiny_flavor, admin_internal_network
    - updated docstrings
    
    Change-Id: Icc1a1c858e8199173956d560c3869733394ad42b

--- stepler/os_faults/fixtures/__init__.py
+++ stepler/os_faults/fixtures/__init__.py
@@ -1,7 +1,7 @@
 """
-----------------
-Neutron fixtures
-----------------
+------------------
+os_faults fixtures
+------------------
 """
 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,23 +17,9 @@ Neutron fixtures
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .neutron import *  # noqa
-from .networks import *  # noqa
-from .routers import *  # noqa
-from .subnets import *  # noqa
+from .faults import *  # noqa
 
 __all__ = sorted([  # sort for documentation
-    'create_network',
-    'network',
-    'public_network',
-    'create_subnet',
-    'subnet',
-    'create_router',
-    'router',
-    'add_router_interfaces',
-    'neutron_client',
-    'network_steps',
-    'router_steps',
-    'subnet_steps',
-    'admin_internal_network'
+    'os_faults_client',
+    'os_faults_steps'
 ])
