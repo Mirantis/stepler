eed431295f1f60ed106d1ba83c4ceb0536673390
Sergey Sokolov <ssm@mera.ru>
2016-10-04 13:45:40 +0000

    Keystone tests
    - new test test_restart_all_services
    - new fixtures and steps for groups
    - new steps for domains
    - taking into account domains and groups in fixtures and steps for users
    - new fixtures tiny_flavor, admin_internal_network
    - updated docstrings
    
    Change-Id: Icc1a1c858e8199173956d560c3869733394ad42b

--- stepler/config.py
+++ stepler/config.py
@@ -65,6 +65,7 @@ CURRENT_CINDER_VERSION = '2'
 NOVA_CONFIG_PATH = '/etc/nova/nova.conf'
 
 # SERVICES
+KEYSTONE = 'keystone'
 NOVA_API = 'nova-api'
 NOVA_COMPUTE = 'nova-compute'
 
@@ -90,6 +91,9 @@ VOLUME_ATTACH_TIMEOUT = 60
 
 # Glance
 IMAGE_AVAILABLE_TIMEOUT = 5 * 60
+
+# Neutron
+ADMIN_INTERNAL_NETWORK_NAME = 'admin_internal_net'
 
 # Nova
 CREDENTIALS_PREFIX = 'stepler_credentials_'
