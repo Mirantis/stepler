8bdc050c7fe47b4071c16feb8d4df984ea725026
Sergey Turivniy <turivniy@gmail.com>
2016-10-12 13:58:51 +0300

    Add test for Ironic node-create
    Change-Id: I174605960a5142f9535ea880ecc2f1a4538e7a91

---
+++ stepler/baremetal/steps/node.py
@@ -0,0 +1,62 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from waiting import wait
+
+from stepler.base import BaseSteps
+from stepler.third_party.steps_checker import step
+
+__all__ = [
+    'NodeSteps'
+]
+
+
+class NodeSteps(BaseSteps):
+    """Node steps."""
+
+    @step
+    def create_node(self, driver='fake', check=True, **kwargs):
+        """Step to create node."""
+
+        node = self._client.node.create(driver=driver, **kwargs)
+
+        if check:
+            self.check_node_presence(node)
+
+        return node
+
+    @step
+    def delete_node(self, node, check=True):
+        """Step to delete node."""
+        self._client.node.delete(node.uuid)
+
+        if check:
+            self.check_node_presence(node, present=False)
+
+    @step
+    def check_node_presence(self, node, present=True, timeout=2):
+        """Verify step to check node is present."""
+        def predicate():
+            try:
+                self.get_node(node)
+
+                return present
+            except Exception:
+                return not present
+
+        wait(predicate, timeout_seconds=timeout)
+
+    @step
+    def get_node(self, node):
+        """Step to get node."""
+        return self._client.node.get(node.uuid)
