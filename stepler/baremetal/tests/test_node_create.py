23571229e224b363b4c7f0589a2f997c5e7aa93f
Sergey Turivniy <turivniy@gmail.com>
2016-10-12 13:58:51 +0300

    Add test for Ironic node-create
    Change-Id: I174605960a5142f9535ea880ecc2f1a4538e7a91

---
+++ stepler/baremetal/tests/test_node_create.py
@@ -0,0 +1,19 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+def test_node_create(ironic_node_steps):
+    """Verify that ironic node can be created and deleted."""
+    node = ironic_node_steps.create_ironic_node()
+    ironic_node_steps.delete_ironic_node(node)
