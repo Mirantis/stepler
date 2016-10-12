c27095dabd11b7345b22af71ae5c19100faa4183
Sergey Turivniy <turivniy@gmail.com>
2016-10-12 13:58:51 +0300

    Add test for Ironic node-create
    Change-Id: I174605960a5142f9535ea880ecc2f1a4538e7a91

---
+++ stepler/baremetal/fixtures/ironic.py
@@ -0,0 +1,42 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ironicclient import client as Client
+import pytest
+
+from stepler import config
+
+__all__ = [
+    'ironic_client'
+]
+
+
+@pytest.fixture
+def ironic_client(session):
+    """Fixture to get ironic client."""
+
+    try:
+        index = config.AUTH_URL.index('v')
+        os_auth_url = config.AUTH_URL[:index]
+
+    except Exception:
+        os_auth_url = config.AUTH_URL
+
+    session = {
+        'os_username': config.USERNAME,
+        'os_password': config.PASSWORD,
+        'os_auth_url': os_auth_url,
+        'os_tenant_name': config.PROJECT_NAME
+    }
+
+    return Client.get_client('1', **session)
