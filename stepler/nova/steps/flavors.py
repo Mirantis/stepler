51e96d264618c4859ba71255cb119a09d29ebdc0
Georgy Dyuldin <g.dyuldin@gmail.com>
2016-10-13 10:30:11 +0300

    Add 'LM of VM with data on root and ephemeral disk' test
    Change-Id: I2de1f8587af5a47544254501012d0acb96bc34b7

--- stepler/nova/steps/flavors.py
+++ stepler/nova/steps/flavors.py
@@ -32,10 +32,34 @@ class FlavorSteps(BaseSteps):
     """Flavor steps."""
 
     @step
-    def create_flavor(self, flavor_name, ram, vcpus, disk, check=True):
-        """Step to create flavor."""
-        flavor = self._client.create(flavor_name, ram=ram, vcpus=vcpus,
-                                     disk=disk)
+    def create_flavor(self, flavor_name, ram, vcpus, disk, flavorid='auto',
+                      ephemeral=0, swap=0, rxtx_factor=1.0, is_public=True,
+                      check=True):
+        """Step to create flavor.
+
+        Args:
+            name (str): Descriptive name of the flavor
+            ram (int): Memory in MB for the flavor
+            vcpus (int): Number of VCPUs for the flavor
+            disk (int): Size of local disk in GB
+            flavorid (str): ID for the flavor (optional). You can use the
+                            reserved value ``"auto"`` to have Nova generate a
+                            UUID for the flavor in cases where you cannot
+                            simply pass ``None``.
+            swap (int): Swap space in MB
+            rxtx_factor (float): RX/TX factor
+        Retuns:
+            object: flavor object
+        """
+        flavor = self._client.create(flavor_name,
+                                     ram=ram,
+                                     vcpus=vcpus,
+                                     disk=disk,
+                                     flavorid=flavorid,
+                                     ephemeral=ephemeral,
+                                     swap=swap,
+                                     rxtx_factor=rxtx_factor,
+                                     is_public=is_public, )
 
         if check:
             self.check_flavor_presence(flavor)
