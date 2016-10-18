177e7727abf6107150d0257d91dda69673a4b8d1
Alexander Koryagin <akoryag@mera.ru>
2016-10-18 12:33:40 +0000

    Nova test 'Launch VM from volume using all standard flavors'
    Steps:
    1. Get list of all flavors
    2. Create and boot server from volume using every flavor
    3. Check that server status is active
    4. Assign floating IP to server
    5. Check with ping that server is available
    
    Change-Id: I9e842502b0b6d515449e3d5a9001e30e621ec191

--- stepler/nova/tests/test_create_vms.py
+++ stepler/nova/tests/test_create_vms.py
@@ -58,3 +58,58 @@ def test_launch_vm_from_image_using_all_flavors(
                 security_groups=[security_group]):
             # check for server state is inside 'create_server_context'
             pass
+
+
+@pytest.mark.testrail_id('1680618')
+def test_launch_vm_from_volume_using_all_flavors(
+        security_group, internal_network, cirros_image, keypair,
+        nova_floating_ip, create_server_context, create_volume,
+        flavor_steps, server_steps):
+    """**Scenario:** Launch VM from volume using all standard flavors.
+
+    This test verifies test #1680618
+
+    **Setup:**
+        #. Upload cirros image
+        #. Create security group with allow ping rule
+        #. Create keypair
+        #. Create volume from cirros image
+    **Steps:**
+        #. Get list of all flavors
+        #. Create and boot server from volume using every flavor
+        #. Check that server status is active
+        #. Assign floating IP to server
+        #. Check that server is available via ping
+    **Teardown:**
+        #. Delete servers
+        #. Delete security group
+        #. Delete keypair
+        #. Delete cirros image
+        #. Delete volume
+        #. Delete floating IP
+    """
+    flavors = flavor_steps.get_flavors()
+
+    volume_name = next(generate_ids(prefix='volume'))
+    volume = create_volume(
+        name=volume_name,
+        size=1,  # GB
+        image=cirros_image,
+        volume_type=None,
+        description=None,
+        check=True)
+
+    for flavor in flavors:
+        server_name = next(generate_ids(prefix='server', postfix=flavor.name))
+
+        with create_server_context(
+                server_name=server_name,
+                image=None,
+                block_device_mapping={'vda': volume.id},
+                flavor=flavor,
+                networks=[internal_network],
+                keypair=keypair,
+                security_groups=[security_group]) as vm:
+
+            server_steps.attach_floating_ip(vm, nova_floating_ip)
+            server_steps.check_ping_to_server_floating(vm, timeout=5 * 60)
