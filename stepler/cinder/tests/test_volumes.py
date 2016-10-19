7274b0629294a54a82560d0ab1caaec7e7077310
Sergey Turivniy <turivniy@gmail.com>
2016-10-19 12:07:11 +0000

    Add test for Cinder to create and delete 10 volumes
    Change-Id: Iba83743e41f05f7ccc3c6254dffe73b3158d1901

---
+++ stepler/cinder/tests/test_volumes.py
@@ -0,0 +1,36 @@
+"""
+------------
+Volume tests
+------------
+"""
+
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import pytest
+
+from stepler.third_party import utils
+
+
+@pytest.mark.testrail_id('965cb50a-2900-4788-974f-9def0648484a')
+def test_create_delete_10_volumes(cinder_steps):
+    """**Scenario:** Verify that 10 cinder volumes can be created and deleted.
+
+    **Steps:**
+
+    #. Create 10 cinder volumes
+    #. Delete 10 cinder volumes
+    """
+    volumes_names = utils.generate_ids('volume', count=10)
+
+    volumes = cinder_steps.create_volumes(names=volumes_names)
+    cinder_steps.delete_volumes(volumes)
