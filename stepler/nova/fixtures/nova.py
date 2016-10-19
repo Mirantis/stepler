1c4d94ec285e04f359985614f5593a7181b7f57c
Georgy Dyuldin <g.dyuldin@gmail.com>
2016-10-19 17:36:18 +0300

    Add WA for bug #1589460
    This bug affected nova live migration cases with instances booted from
    volume.
    
    Change-Id: Iab994a7ebd028368a65351db6c8225222c41c922

--- stepler/nova/fixtures/nova.py
+++ stepler/nova/fixtures/nova.py
@@ -20,9 +20,12 @@ Nova fixtures
 from novaclient.client import Client
 import pytest
 
+from stepler import config
+
 __all__ = [
     'get_nova_client',
     'nova_client',
+    'disable_nova_config_drive',
 ]
 
 
@@ -54,3 +57,32 @@ def nova_client(get_nova_client):
         novaclient.client.Client: authenticated nova client
     """
     return get_nova_client()
+
+
+@pytest.yield_fixture(scope='module')
+def disable_nova_config_drive(patch_ini_file_and_restart_services,
+                              get_availability_zone_steps):
+    """Module fixture to disable nova config drive.
+
+    Note:
+        Workaround for bug https://bugs.launchpad.net/mos/+bug/1589460/
+        This should be removed in MOS 10.0
+
+    Args:
+        patch_ini_file_and_restart_services (function): callable fixture to
+            patch ini file and restart services
+        get_availability_zone_steps (function): callable fixture to get
+            availability zone steps
+    """
+    with patch_ini_file_and_restart_services(
+            [config.NOVA_API, config.NOVA_COMPUTE],
+            file_path=config.NOVA_CONFIG_PATH,
+            option='force_config_drive',
+            value=False):
+        zone_steps = get_availability_zone_steps()
+        zone_steps.check_all_active_hosts_available()
+
+        yield
+
+    zone_steps = get_availability_zone_steps()
+    zone_steps.check_all_active_hosts_available()
