711541a029e7b350b3fa516b5dddc8b9a1e477e5
Sergey Turivniy <turivniy@gmail.com>
2016-10-20 16:28:42 +0000

    Add tests for creating images from volumes
    Change-Id: I711c68aa068c69988e0eb6b671f5bc6a07c030dd

---
+++ stepler/cinder/tests/test_create_image_from_volume.py
@@ -0,0 +1,37 @@
+"""
+------------
+Volume tests
+------------
+"""
+
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import pytest
+
+from stepler.third_party import utils
+
+
+@pytest.mark.parametrize('disk_format', ['raw', 'qcow2'])
+@pytest.mark.idempotent_id('7a8f8745-0348-458c-8cf6-143b4627276a')
+def test_create_image_from_volume(upload_volume_to_image, disk_format):
+    """**Scenario:** Verify that raw|qcow2 image is created from volume.
+    **Steps:**
+    #. Create cinder volume
+    #. Create raw|qcow2 image from volume
+    #. Delete raw|qcow2 image
+    #. Delete cinder volume
+    """
+    volume_name = next(utils.generate_ids('volume'))
+    image_name = next(utils.generate_ids('image'))
+
+    upload_volume_to_image(volume_name, image_name, disk_format=disk_format)
