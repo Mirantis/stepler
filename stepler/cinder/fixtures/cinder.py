3af98ef94ff2c7a899a04bc41f247ce4c70ba5be
Sergey Turivniy <turivniy@gmail.com>
2016-10-20 16:28:42 +0000

    Add tests for creating images from volumes
    Change-Id: I711c68aa068c69988e0eb6b671f5bc6a07c030dd

--- stepler/cinder/fixtures/cinder.py
+++ stepler/cinder/fixtures/cinder.py
@@ -21,12 +21,14 @@ from cinderclient import client as cinderclient
 import pytest
 
 from stepler.cinder import steps
+from stepler import config
 
 __all__ = [
     'create_volume',
     'create_volumes',
     'cinder_client',
     'cinder_steps',
+    'upload_volume_to_image',
 ]
 
 
@@ -58,7 +60,7 @@ def cinder_steps(cinder_client):
 
 @pytest.yield_fixture
 def create_volumes(cinder_steps):
-    """Function fixture to create volumes with options.
+    """Callable function fixture to create volumes with options.
 
     Can be called several times during a test.
     After the test it destroys all created volumes.
@@ -84,7 +86,7 @@ def create_volumes(cinder_steps):
 
 @pytest.fixture
 def create_volume(create_volumes):
-    """Function fixture to create single volume with options.
+    """Callable function fixture to create single volume with options.
 
     Can be called several times during a test.
     After the test it destroys all created volumes.
@@ -99,3 +101,37 @@ def create_volume(create_volumes):
         return create_volumes([name], *args, **kwgs)[0]
 
     return _create_volume
+
+
+@pytest.yield_fixture
+def upload_volume_to_image(create_volume, cinder_steps, glance_steps):
+    """Callable function fixture to upload volume to image.
+
+    Can be called several times during a test.
+    After the test it destroys all created objects.
+
+    Args:
+        create_volume (function): function to create volume with options
+        cinder_steps (CinderSteps): instantiated cinder steps
+        glance_steps (GlanceSteps): instantiated glance steps
+
+    Returns:
+        function: function to upload volume to image
+    """
+    images = []
+
+    def _upload_volume_to_image(volume_name, image_name, disk_format):
+        volume = create_volume(volume_name)
+        image_info = cinder_steps.volume_upload_to_image(
+            volume=volume, image_name=image_name, disk_format=disk_format)
+        image = glance_steps.get_image(
+            image_info['os-volume_upload_image']['image_id'])
+        images.append(image)
+        glance_steps.check_image_status(image, status='active',
+                                        timeout=config.IMAGE_AVAILABLE_TIMEOUT)
+        return image
+
+    yield _upload_volume_to_image
+
+    if images:
+        glance_steps.delete_images(images)
