7579b2482c085b3686dd5ad661333ff2124d23e7
Kyrylo Romanenko <kromanenko@mirantis.com>
2016-10-20 17:35:17 +0300

    Add tests for Cinder volumes
    Tests:
    C2060973    Create volume without name
    C2060974    Create volume with name lenght > 256
    C2060975    Create volume with description lenght == max(255)
    C2060976    Create volume with description
    
    Change-Id: I571c99855be43cedc6bd246811f1a5133bb399a0

--- stepler/cinder/tests/test_volumes.py
+++ stepler/cinder/tests/test_volumes.py
@@ -17,6 +17,9 @@ Volume tests
 #    under the License.
 
 import pytest
+import six
+
+from cinderclient import exceptions
 
 from stepler.third_party import utils
 
@@ -34,3 +37,54 @@ def test_create_delete_10_volumes(cinder_steps):
 
     volumes = cinder_steps.create_volumes(names=volumes_names)
     cinder_steps.delete_volumes(volumes)
+
+
+@pytest.mark.idempotent_id('45783965-096f-46d6-a863-e466cc9d2d49')
+def test_create_volume_without_name(cinder_steps):
+    """**Scenario:** Verify creation of volume without name
+
+    **Steps:**
+
+    #. Create volume without name
+    """
+    cinder_steps.create_volume()
+
+
+@pytest.mark.idempotent_id('bcd12002-dfd3-44c9-b270-d844d61a009c')
+def test_create_volume_long_name(cinder_steps):
+    """**Scenario:** Verify creation of volume with name length > 255
+
+    **Steps:**
+
+    #. Create volume with name length > 255
+    """
+    long_name = utils.generate_ids(length=256)
+    ex_text = "Name has more than 255 characters"
+
+    six.assertRaisesRegex(exceptions.BadRequest, ex_text,
+                          cinder_steps.create_volume,
+                          name=long_name)
+
+
+@pytest.mark.idempotent_id('8b08bc8f-e1f4-4f6e-8f98-dfcb1f9f538a')
+def test_create_volume_description(cinder_steps):
+    """**Scenario:** Verify creation of volume with description.
+
+    **Steps:**
+
+    #. Create volume with description
+    """
+    description = utils.generate_ids(prefix='volume')
+    cinder_steps.create_volume(description=description)
+
+
+@pytest.mark.idempotent_id('56cc7c76-ae92-423d-81ad-8cece5f875ad')
+def test_create_volume_description_max(cinder_steps):
+    """**Scenario:** Verify creation of volume with max description length.
+
+    **Steps:**
+
+    #. Create volume with description length == max(255)
+    """
+    description = utils.generate_ids(prefix='volume', length=255)
+    cinder_steps.create_volume(description=description)
