9e281ab9bc354711fe6408b3fce3f9545ad80f0c
Alexander Koryagin <akoryag@mera.ru>
2016-10-21 08:57:33 +0000

    Put metadata on all instances scheduled on a single compute node
    Actions:
    1. Create 2 nova servers on host 1.
    2. Create 2 nova servers on host 2.
    3. From controller node add new 'key=value' to metadata of servers, located on host 1.
    4. Check that servers from host 1 have 'key=value' in their metadata.
    5. Check that servers from host 2 do NOT have 'key=value' in their metadata.
    6. From controller node delete 'key=value' from metadata of servers, located on host 1.
    7. Check that servers from host 1 and host 2 do NOT have 'key=value' in their metadata.
    
    Change-Id: Ib15d5297ff99024baf105c54168c567e04a09e3e

---
+++ stepler/nova/fixtures/zones.py
@@ -0,0 +1,51 @@
+"""
+-------------
+Zone fixtures
+-------------
+"""
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+
+from stepler.nova import steps
+
+__all__ = [
+    'zone_steps',
+    'nova_zone',
+    'nova_zone_hosts'
+]
+
+
+@pytest.fixture
+def zone_steps(nova_client):
+    """Fixture to get zone steps."""
+    return steps.ZoneSteps(nova_client.availability_zones)
+
+
+@pytest.fixture
+def nova_zone(zone_steps):
+    """Fixture to get one available nova zone object."""
+    return zone_steps.get_zone(**{'zoneName': 'nova',
+                                  'zoneState': {'available': True}})
+
+
+@pytest.fixture
+def nova_zone_hosts(nova_zone):
+    """Fixture to get all hosts from nova zone.
+
+    Returns:
+        list: str FQDN values
+    """
+    return nova_zone.hosts.keys()
