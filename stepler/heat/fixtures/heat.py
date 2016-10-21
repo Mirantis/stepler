71661ffe9c753b075ed2af118a5278980fb3e678
Georgy Dyuldin <g.dyuldin@gmail.com>
2016-10-21 16:35:22 +0300

    Add 'Create stack with AWS resources' test
    Change-Id: I4e2c163cc3ee3db9822345516951adbcc4189ff4

---
+++ stepler/heat/fixtures/heat.py
@@ -0,0 +1,42 @@
+"""
+-------------
+Heat fixtures
+-------------
+"""
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from heatclient import client as heatclient
+import pytest
+
+from stepler import config
+
+__all__ = ['heat_client',]
+
+
+@pytest.fixture
+def heat_client(session):
+    """Function fixture to get heat client.
+
+    Args:
+        session (object): authenticated keystone session
+
+    Returns:
+        heatclient.Client: instantiated heat client
+    """
+    token = session.get_token()
+    endpoint_url = session.get_endpoint(
+        service_type='orchestration', endpoint_type='publicURL')
+    return heatclient.Client(
+        version=config.HEAT_VERSION, endpoint=endpoint_url, token=token)
