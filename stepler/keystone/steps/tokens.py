8bdf426e4736ad572a4b03b8bb499c5d1b5ee982
Sergey Turivniy <turivniy@gmail.com>
2016-10-24 16:46:10 +0000

    Add steps for token get_token_data and validate
    Change-Id: I5942aed296c20238cd3d2b1856b1ee600228690a

---
+++ stepler/keystone/steps/tokens.py
@@ -0,0 +1,86 @@
+"""
+---------------
+Keystone token steps
+---------------
+"""
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from hamcrest import assert_that, has_key, has_items  # noqa
+
+from stepler.base import BaseSteps
+from stepler.third_party.steps_checker import step
+
+__all__ = [
+    'TokenSteps'
+]
+
+
+class TokenSteps(BaseSteps):
+    """Token steps."""
+
+    @step
+    def get_token_data(self, token, include_catalog=True, check=True):
+        """Step to fetch the data about a token from the identity server.
+
+        Args:
+            token (str): The ID of the token to be fetched
+            include_catalog (bool): Whether the service catalog should be
+                                    included in the response.
+            check (bool): flag whether to check step or not
+
+        Returns:
+            dict: data about the token
+        """
+        token_data = self._client.get_token_data(
+            token=token,
+            include_catalog=include_catalog)
+
+        if check:
+            assert_that(token_data, has_key('token'))
+
+        return token_data
+
+    @step
+    def get_token_validate(self, token, include_catalog=True, check=True):
+        """Step to validate a token.
+
+        Args:
+            token (str): The ID of the token to be fetched
+            include_catalog (bool): Whether the service catalog should be
+                                    included in the response.
+            check (bool): flag whether to check step or not
+
+        Returns:
+            class: keystoneclient.access.AccessInfoV3
+        """
+        token_validate = self._client.validate(
+            token=token,
+            include_catalog=include_catalog)
+
+        if check:
+            assert_that(token_validate.keys(), has_items('methods',
+                                                         'roles',
+                                                         'auth_token',
+                                                         'expires_at',
+                                                         'project',
+                                                         'catalog',
+                                                         'version',
+                                                         'user',
+                                                         'audit_ids',
+                                                         'issued_at'))
+
+        return token_validate
+
+
