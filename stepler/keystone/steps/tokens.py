80dd9e048e699bd977db331a1ca9e396aff16853
Sergey Turivniy <turivniy@gmail.com>
2016-10-25 15:44:36 +0000

    Add steps for keystone to revoke tokens
    Change-Id: I33454da424b6627faa807364108c24217b8a9adf

--- stepler/keystone/steps/tokens.py
+++ stepler/keystone/steps/tokens.py
@@ -17,10 +17,14 @@ Keystone token steps
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from hamcrest import assert_that, has_key, has_items  # noqa
+from hamcrest import assert_that, has_key, has_items, is_not  # noqa
+from keystoneclient import exceptions
+from waiting import wait
 
 from stepler import base
+from stepler.third_party import expect_that
 from stepler.third_party import steps_checker
+from stepler.third_party import waiter
 
 __all__ = [
     'TokenSteps'
@@ -29,6 +33,48 @@ __all__ = [
 
 class TokenSteps(base.BaseSteps):
     """Token steps."""
+
+    @steps_checker.step
+    def revoke_token(self, token, check=True):
+        """Step to revoke a token.
+
+        Args:
+            token (str): The token to be revoked.
+            check (bool): flag whether to check step or not
+
+        Returns:
+            keystoneclient.access.AccessInfo: token
+        """
+        revoked_token = self._client.revoke_token(token=token)
+
+        if check:
+            self.check_token_is_revoked(revoked_token, must_revoked=False)
+
+        return revoked_token
+
+    @steps_checker.step
+    def check_token_is_revoked(self, token, must_revoked=True, timeout=0):
+        """Step to check if token is revoked.
+
+        Args:
+            token (str): The token to be checked.
+            must_revoked (bool): flag whether volume should present or not
+            timeout (int): seconds to wait a result of check
+
+        Raises:
+            TimeoutExpired: if check was triggered to an error after timeout
+        """
+        def predicate():
+            try:
+                self.get_token_validate(token)
+                is_revoked = True
+
+            except exceptions.NotFound:
+                is_revoked = False
+
+            return expect_that(is_revoked, equal_to(must_revoked))
+
+        waiter.wait(predicate, timeout_seconds=timeout)
 
     @steps_checker.step
     def get_token_data(self, token, include_catalog=True, check=True):
