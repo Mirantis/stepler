6e6a9e687e65b5fcb26d3061a848997f480e373e
Alexandra Allakhverdieva <aallakhverdieva@mirantis.com>
2016-10-26 13:43:36 +0000

    Add tests for volume type
    test_change_volume_type_from_empty
    test_change_volume_type
    test_negative_create_volume_non_exist_volume_type
    
    Change-Id: I78ebe1b9585a78e65d76389cddfeacfe00aa30e2

---
+++ stepler/cinder/tests/test_change_volume_type.py
@@ -0,0 +1,70 @@
+"""
+------------
+Volume tests
+------------
+"""
+
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import pytest
+
+from stepler import config
+from stepler.third_party import utils
+
+
+@pytest.mark.idempotent_id('43b96116-a0ba-45cc-b584-af9ee838ba49')
+def test_change_volume_type_from_empty(volume_type,
+                                       create_volume,
+                                       volume_steps):
+    """**Scenario:** Verify change volume type from empty value
+
+    **Steps:**
+
+    #. Create cinder volume without volume type
+    #. Create new volume type
+    #. Change volume type to newly created type
+
+    **Teardown:**
+
+    #. Delete volume
+    #. Delete volume type
+    """
+    volume = create_volume()
+    volume_steps.change_volume_type(volume, volume_type, config.POLICY_NEVER)
+
+
+@pytest.mark.idempotent_id('a5cee93e-6d58-420e-a61c-85cbd10d6725')
+def test_change_volume_type(create_volume_type,
+                            create_volume,
+                            volume_steps,
+                            volume_type_steps):
+    """**Scenario:** Verify change volume type
+
+    **Steps:**
+
+    #. Create two volume types
+    #. Create volume using first volume type
+    #. Retype volume to another type
+
+    **Teardown:**
+
+    #. Delete volume
+    #. Delete volume types
+    """
+    volume_type_name_1 = next(utils.generate_ids('volume_type'))
+    volume_type_name_2 = next(utils.generate_ids('volume_type'))
+    volume_type_1 = create_volume_type(volume_type_name_1)
+    volume_type_2 = create_volume_type(volume_type_name_2)
+
+    volume = create_volume(volume_type=volume_type_1.name)
+    volume_steps.change_volume_type(volume, volume_type_2, config.POLICY_NEVER)
