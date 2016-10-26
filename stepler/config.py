89bf18ee6846e28cf9af43086c9de3eed6babfe5
Georgy Dyuldin <g.dyuldin@gmail.com>
2016-10-26 15:55:24 +0300

    Add heat CLI test for creating stack from URL
    Change-Id: I8bcb99feed318fd6e7a3d4aad7cc6b15e5264565

--- stepler/config.py
+++ stepler/config.py
@@ -136,3 +136,4 @@ HEAT_COMPLETE_STATUS = 'complete'
 HEAT_COMPLETE_CLI_STATUS = 'create_complete'
 STACK_CREATION_TIMEOUT = 5 * 60
 STACK_DELETING_TIMEOUT = 3 * 60
+HEAT_SIMPLE_TEMPLATE_URL = 'https://raw.githubusercontent.com/openstack/heat-templates/master/hot/resource_group/resource_group.yaml'  # noqa
