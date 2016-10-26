a4f37ff1932135ad5753142676067a5625258e2e
Georgy Dyuldin <g.dyuldin@gmail.com>
2016-10-26 15:55:24 +0300

    Add heat CLI test for creating stack from URL
    Change-Id: I8bcb99feed318fd6e7a3d4aad7cc6b15e5264565

--- stepler/config.py
+++ stepler/config.py
@@ -155,6 +155,7 @@ STACK_CREATION_TIMEOUT = 5 * 60
 STACK_DELETING_TIMEOUT = 3 * 60
 STACK_UPDATING_TIMEOUT = 5 * 60
 RESOURCE_NAME = 'stepler_cirros_image'
+HEAT_SIMPLE_TEMPLATE_URL = 'https://raw.githubusercontent.com/openstack/heat-templates/master/hot/resource_group/resource_group.yaml'  # noqa
 
 # CLI clients
 SERVER_LIST_TIMEOUT = 60
