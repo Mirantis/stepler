7458ecd3eb63b611471cb57a6a64d60645c8e16c
Alexey Zaytsev <azaytsev@mirantis.com>
2016-10-26 14:37:19 +0300

    Test "Dispatch an external event"
    1. Create instance with new net and subnet, boot it.
    2. Check in nova-api log that the external event
       "network-vif-plugged" have been created for this
       instance and got "status": "completed".
    
    Change-Id: Ia4e22d3df6c3ce6602fab08238db29bd7890a8ac

---
+++ stepler/nova/tests/test_dispatch_ext_events.py
@@ -0,0 +1,73 @@
+"""
+--------------------------------
+Dispatch an external event tests
+--------------------------------
+"""
+
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from stepler import config
+from stepler.third_party import utils
+
+EVENT_RECORD = 'server_external_events.*status.*completed' + \
+               '.*name.*network-vif-plugged.*server_uuid.*'
+
+
+def test_dispatch_external_event(
+        cirros_image,
+        flavor,
+        security_group,
+        create_network,
+        create_subnet,
+        create_server,
+        os_faults_steps):
+    """**Scenario:** Dispatch an external event
+
+    **Setup:**
+
+        #. Create cirros image
+        #. Create network
+        #. Create subnet
+        #. Create server
+
+    **Steps:**
+
+        #. Check in nova-api log that the external event "network-vif-plugged"
+           have been created for this instance and got "status": "completed"
+
+    **Teardown:**
+
+        #. Delete server
+        #. Delete subnet
+        #. Delete network
+        #. Delete cirros image
+    """
+    network_name = next(utils.generate_ids('net01'))
+    network = create_network(network_name)
+    subnet_name = next(utils.generate_ids('subnet'))
+    create_subnet(subnet_name, network=network, cidr='192.168.1.0/24')
+
+    server_name = next(utils.generate_ids('server'))
+    server = create_server(
+        server_name,
+        image=cirros_image,
+        flavor=flavor,
+        networks=[network],
+        security_groups=[security_group])
+
+    nodes = os_faults_steps.get_nodes(service_names=[config.NOVA_API])
+
+    os_faults_steps.check_file_contains_line(nodes,
+                                             config.NOVA_API_LOG_FILE,
+                                             EVENT_RECORD + server.id,
+                                             all=False)
