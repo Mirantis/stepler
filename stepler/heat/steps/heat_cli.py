48cde942a4953c85aa9a23ad4a9820e8527ed2b9
Georgy Dyuldin <g.dyuldin@gmail.com>
2016-10-26 15:55:24 +0300

    Add heat CLI test for creating stack from URL
    Change-Id: I8bcb99feed318fd6e7a3d4aad7cc6b15e5264565

--- stepler/heat/steps/heat_cli.py
+++ stepler/heat/steps/heat_cli.py
@@ -51,7 +51,8 @@ class HeatCLISteps(base.BaseSteps):
     def create_stack(self,
                      node,
                      name,
-                     template_file,
+                     template_file=None,
+                     template_url=None,
                      parameters=None,
                      check=True):
         """Step to create stack.
@@ -59,14 +60,22 @@ class HeatCLISteps(base.BaseSteps):
         Args:
             node (obj): os-fault node to execute command on it
             name (str): name of stack
-            template_file (str): path to yaml template
+            template_file (str, optional): path to yaml template
+            template_url (str, optional): template url
             parameters (dict|None): parameters for template
             check (bool): flag whether check step or not
 
         Returns:
             dict: heat stack
         """
-        cmd = 'heat stack-create {} -f {}'.format(name, template_file)
+        assert any([template_file, template_url]), ('One of `template_file` '
+                                                    'or `template_url` should '
+                                                    'be passed.')
+        cmd = 'heat stack-create ' + name
+        if template_file:
+            cmd += ' -f ' + template_file
+        elif template_url:
+            cmd += ' -u ' + template_url
         parameters = parameters or {}
         for key, value in parameters.items():
             cmd += ' --parameters {}={}'.format(key, value)
