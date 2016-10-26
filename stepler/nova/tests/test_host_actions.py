d52dd04fceea21ecd7a139dee9996f7c44cd4891
Sergey Sokolov <ssm@mera.ru>
2016-10-26 11:27:44 +0300

    Nova: Get info about resources’ usage on nodes
    - new test
    - new fixture and steps for nova.hosts
    - minor updates of test_migrate_instances
    
    Change-Id: I0082818ecd9f69e20548f191845f2714c9cfff99

--- stepler/nova/tests/test_host_actions.py
+++ stepler/nova/tests/test_host_actions.py
@@ -1,10 +1,9 @@
 """
------------------------------
+-----------------------
 Nova host actions tests
------------------------------
+-----------------------
 """
-#    Copyright 2016 Mirantis, Inc.
-#
+
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
 #    a copy of the License at
@@ -17,14 +16,120 @@ Nova host actions tests
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import pytest
+
 from stepler import config
 from stepler.third_party import utils
 
 
-def test_migrate_instances(
-        cirros_image, network, subnet, router, security_group, flavor,
-        add_router_interfaces, keypair, hypervisor_steps, create_servers,
-        server_steps, nova_create_floating_ip):
+@pytest.mark.idempotent_id('2eb54a35-7218-4220-b376-8aa5f1b6f74f')
+def test_resources_info(cirros_image,
+                        flavor,
+                        network,
+                        subnet,
+                        keypair,
+                        security_group,
+                        create_servers,
+                        hypervisor_steps,
+                        host_steps):
+    """**Scenario:** Get info about resources' usage on nodes
+
+    **Setup:**
+
+        #. Create cirros image
+        #. Create flavor
+        #. Create net and subnet
+        #. Create keypair
+        #. Create security group
+
+    **Steps:**
+
+        #. Get resource info for node-1 and node-2
+        #. Create two instances on node-1
+        #. Get resource info for node-1 and check that resource usage is
+           changed and project_id appears in results
+        #. Get resource info for node-2 and check that resource usage is
+           not changed
+        #. Create two instances on node-2
+        #. Get resource info for node-1 and check that resource usage is
+           not changed
+        #. Get resource info for node-2 and check that resource usage is
+           changed and project_id appears in results
+
+    **Teardown:**
+
+        #. Delete instances
+        #. Delete security group
+        #. Delete keypair
+        #. Delete net and subnet
+        #. Delete flavor
+        #. Delete cirros image
+    """
+    hypervisors = hypervisor_steps.get_hypervisors(min_number=2)
+    host_name_1 = hypervisors[0].hypervisor_hostname
+    host_name_2 = hypervisors[1].hypervisor_hostname
+
+    host_1 = host_steps.get_host(host_name_1)
+    host_2 = host_steps.get_host(host_name_2)
+    usage_data_1_old = host_steps.get_usage_data(host_1)
+    usage_data_2_old = host_steps.get_usage_data(host_2)
+
+    server_names = utils.generate_ids(prefix='server', count=2)
+    availability_zone = 'nova:' + host_name_1
+    servers = create_servers(server_names, cirros_image, flavor,
+                             networks=[network],
+                             keypair=keypair,
+                             security_groups=[security_group],
+                             availability_zone=availability_zone,
+                             username=config.CIRROS_USERNAME)
+
+    usage_data_1_new = host_steps.get_usage_data(host_1)
+    host_steps.check_usage_difference(usage_data_1_old,
+                                      usage_data_1_new,
+                                      changed=True,
+                                      project_id=servers[0].tenant_id)
+
+    usage_data_2_new = host_steps.get_usage_data(host_2)
+    host_steps.check_usage_difference(usage_data_2_old,
+                                      usage_data_2_new,
+                                      changed=False)
+
+    server_names = utils.generate_ids(prefix='server', count=2)
+    availability_zone = 'nova:' + host_name_2
+    servers = create_servers(server_names, cirros_image, flavor,
+                             networks=[network],
+                             keypair=keypair,
+                             security_groups=[security_group],
+                             availability_zone=availability_zone,
+                             username=config.CIRROS_USERNAME)
+
+    usage_data_1_old = usage_data_1_new
+    usage_data_1_new = host_steps.get_usage_data(host_1)
+    host_steps.check_usage_difference(usage_data_1_old,
+                                      usage_data_1_new,
+                                      changed=False)
+
+    usage_data_2_old = usage_data_2_new
+    usage_data_2_new = host_steps.get_usage_data(host_2)
+    host_steps.check_usage_difference(usage_data_2_old,
+                                      usage_data_2_new,
+                                      changed=True,
+                                      project_id=servers[0].tenant_id)
+
+
+@pytest.mark.idempotent_id('ffc320c3-5688-4442-bcc5-05ae51788d2e')
+def test_migrate_instances(cirros_image,
+                           network,
+                           subnet,
+                           router,
+                           security_group,
+                           flavor,
+                           add_router_interfaces,
+                           keypair,
+                           hypervisor_steps,
+                           create_servers,
+                           server_steps,
+                           nova_create_floating_ip):
     """**Scenario:** Migrate instances from the specified host to other hosts
 
     **Setup:**
@@ -68,7 +173,7 @@ def test_migrate_instances(
                              keypair=keypair,
                              security_groups=[security_group],
                              availability_zone=availability_zone,
-                             username='cirros')
+                             username=config.CIRROS_USERNAME)
     server_steps.migrate_servers(servers)
     server_steps.confirm_resize_servers(servers)
 
