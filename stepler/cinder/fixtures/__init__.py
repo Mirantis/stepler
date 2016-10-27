6b514153cc5029fc593a2d2a13b4fdca6222f20d
Georgy Dyuldin <g.dyuldin@gmail.com>
2016-10-27 12:12:11 +0300

    Add Cinder test to check creating of many snapshots
    Change-Id: I0fea2bb315c0fffd1ed8b6a039797e6477081e98

--- stepler/cinder/fixtures/__init__.py
+++ stepler/cinder/fixtures/__init__.py
@@ -17,12 +17,12 @@ Cinder fixtures
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .volumes import *  # noqa
 from .volume_types import *  # noqa
 from .cinder import *  # noqa
 from .quota import *  # noqa
 from .snapshots import *  # noqa
 from .transfers import *  # noqa
+from .volumes import *  # noqa
 
 __all__ = sorted([  # sort for documentation
     'create_volume',
@@ -37,6 +37,7 @@ __all__ = sorted([  # sort for documentation
     'volumes_cleanup',
 
     'cinder_quota_steps',
+    'big_snapshot_quota',
 
     'transfer_steps',
     'create_volume_transfer',
--- stepler/cinder/steps/__init__.py
+++ stepler/cinder/steps/__init__.py
@@ -19,14 +19,13 @@ Contains steps specific for cinder.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .volumes import *  # noqa
 from .volume_types import *  # noqa
 from .quota import *  # noqa
 from .snapshots import *  # noqa
+from .volumes import *  # noqa
 from .volume_transfer import *  # noqa
 
 __all__ = [
-    'VolumeSteps',
     'VolumeSteps',
     'VolumeTypeSteps',
     'CinderQuotaSteps',
