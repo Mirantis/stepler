fdf15022fb16919e1827e144967e6cf9927f48ea
Sergey Turivniy <turivniy@gmail.com>
2016-10-28 22:55:03 +0300

    Add docstrings for nova steps keypairs
    Change-Id: Id7b1989efcee6b0263676dbca8b25a9650d50b3b

--- stepler/nova/steps/keypairs.py
+++ stepler/nova/steps/keypairs.py
@@ -17,6 +17,7 @@ Keypair steps
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from hamcrest import assert_that, equal_to  # noqa
 from waiting import wait
 
 from stepler.base import BaseSteps
@@ -31,18 +32,39 @@ class KeypairSteps(BaseSteps):
     """Keypair steps."""
 
     @steps_checker.step
-    def create_keypair(self, keypair_name, check=True):
-        """Step to create keypair."""
-        keypair = self._client.create(keypair_name)
+    def create_keypair(self, keypair_name, public_key=None, check=True):
+        """Step to create keypair.
+
+        Args:
+            keypair_name (str): name for the keypair to create
+            public_key (str): existing public key to import
+
+        Returns:
+            keypair (object): keypair
+
+        Raises:
+            TimeoutExpired: if check was triggered to an error after timeout
+        """
+        keypair = self._client.create(keypair_name, public_key=public_key)
 
         if check:
             self.check_keypair_presence(keypair)
+            if public_key is not None:
+                assert_that(keypair.public_key, equal_to(public_key))
 
         return keypair
 
     @steps_checker.step
     def delete_keypair(self, keypair, check=True):
-        """Step to delete keypair."""
+        """Step to delete keypair.
+
+        Args:
+            keypair (object): key to delete.
+            check (bool): flag whether to check step or not
+
+        Raises:
+            TimeoutExpired: if check was triggered to an error after timeout
+        """
         self._client.delete(keypair.id)
 
         if check:
@@ -50,7 +72,16 @@ class KeypairSteps(BaseSteps):
 
     @steps_checker.step
     def check_keypair_presence(self, keypair, present=True, timeout=0):
-        """Verify step to check keypair is present."""
+        """Verify step to check keypair is present.
+
+        Args:
+            keypair (class or its ID): key to check.
+            present (bool): flag whether image should present or not
+            timeout (int): seconds to wait a result of check
+
+        Raises:
+            TimeoutExpired: if check was triggered to an error after timeout
+        """
         def predicate():
             try:
                 self._client.get(keypair.id)
