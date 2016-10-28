5518d7ed22c44fe3db278fa3fbe427a9f5f9c030
Georgy Dyuldin <g.dyuldin@gmail.com>
2016-10-28 16:13:52 +0300

    Add Cinder UI test to creating backup with long name
    Change-Id: I0b1dd46110875010009c9954857f7c675ba66207

--- stepler/horizon/tests/test_volume_backups.py
+++ stepler/horizon/tests/test_volume_backups.py
@@ -28,12 +28,12 @@ class TestAnyOne(object):
 
     @pytest.mark.idempotent_id('7f43197f-ba2c-4962-a07a-43cbf77a779b')
     def test_volume_backups_pagination(self, create_backups, update_settings,
-                                       volumes_steps):
+                                       volumes_steps_ui):
         """Verify that volume backups pagination works right and back."""
         backup_names = list(generate_ids('backup', count=3))
         create_backups(backup_names)
         update_settings(items_per_page=1)
-        volumes_steps.check_backups_pagination(backup_names)
+        volumes_steps_ui.check_backups_pagination(backup_names)
 
     @pytest.mark.idempotent_id('dd3abd5c-0900-40a0-be3d-b284eeb6b5da')
     def test_create_volume_backup(self, create_backups):
@@ -55,3 +55,24 @@ class TestAnyOne(object):
         """
         backup_name = next(generate_ids('backup'))
         create_backups([backup_name])
+
+    @pytest.mark.idempotent_id('acf12ae6-7b00-4bad-aef4-25ca3e3c3dc0')
+    def test_volume_backup_form_max_name_length(self, volume,
+                                                volumes_steps_ui):
+        """**Scenario:** Create volume backup with name lenght > 255
+
+        **Setup:**
+
+        #. Create volume
+
+        **Steps:**
+
+        #. Check that backup name on backup creation form can contains max
+            255 symbols
+
+        **Teardown:**
+
+        #. Delete volume
+        """
+        volumes_steps_ui.check_backup_creation_form_name_field_max_length(
+            volume.name, 255)
