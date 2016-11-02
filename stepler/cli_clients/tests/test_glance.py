c834118823cc11c843d7dfcefb0ef0abfe466a7b
Alina Nesterova <anesterova@mirantis.com>
2016-11-02 18:11:24 +0300

    Validate Glance unicode support with CLI
    Added check if Glance works with unicode names correctly
    using CLI
    
    Change-Id: I03b7176c0c5e3199af8acd2dc8a1a4ef569df4d5

--- stepler/cli_clients/tests/test_glance.py
+++ stepler/cli_clients/tests/test_glance.py
@@ -1,24 +1,49 @@
+# -*- coding: utf-8 -*-
+
 """
-----------------
-Glance CLI tests
-----------------
+---------------------------
+Tests for glance CLI client
+---------------------------
 """
 
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import pytest
 
+from stepler import config
 from stepler.third_party import utils
+
+
+@pytest.mark.idempotent_id('85461edd-7c82-4c27-ad19-d3af178818fe')
+@pytest.mark.parametrize('api_version', [1, 2])
+def test_validate_unicode_support_in_image_name(glance_steps,
+                                                cli_glance_steps,
+                                                api_version):
+    """** Scenario: ** Check support of unicode symbols in image name.
+
+    ** Steps: **
+
+    #. Create image with name 試験画像
+    #. Check that created image is in list and has status `queued`
+    #. Delete image
+    #. Check that image deleted
+    """
+    glance_steps.image_create(
+        image_name=u"試験画像",
+        image_path=utils.get_file_path(config.CIRROS_QCOW2_URL))
+    cli_glance_steps.check_unicode_support(image_name=u"試験画像",
+                                           api_version=api_version)
 
 
 @pytest.mark.idempotent_id('b5948a9d-a0a2-40ca-9638-554020dcde80',
