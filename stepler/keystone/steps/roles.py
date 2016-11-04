cc9728897e69d58d4cef3f4a4e4bf7e3b6435750
Sergey Turivniy <turivniy@gmail.com>
2016-11-04 15:08:50 +0200

    Add dockstrings to keystone steps roles
    Change-Id: Id661660594f63255c22d18f33cdf9d2006117e90

--- stepler/keystone/steps/roles.py
+++ stepler/keystone/steps/roles.py
@@ -17,10 +17,13 @@ Role steps
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from waiting import wait
+from hamcrest import equal_to  # noqa
+from keystoneclient import exceptions
 
 from stepler.base import BaseSteps
+from stepler.third_party.matchers import expect_that
 from stepler.third_party import steps_checker
+from stepler.third_party import waiter
 
 __all__ = [
     'RoleSteps'
@@ -32,7 +35,18 @@ class RoleSteps(BaseSteps):
 
     @steps_checker.step
     def create_role(self, role_name, check=True):
-        """Step to create role."""
+        """Step to create role.
+
+        Args:
+           role_name (str): the name of the role
+           check (bool): flag whether to check step or not
+
+        Returns:
+            keystoneclient.v3.roles.Role: new role
+
+        Raises:
+            TimeoutExpired|AssertionError: if check was triggered to an error
+        """
         role = self._client.create(role_name)
 
         if check:
@@ -41,59 +55,180 @@ class RoleSteps(BaseSteps):
 
     @steps_checker.step
     def delete_role(self, role, check=True):
-        """Step to delete role."""
+        """Step to delete role.
+
+        Args:
+            role (object): role
+            check (bool): flag whether to check step or not
+        """
         self._client.delete(role.id)
         if check:
             self.check_role_presence(role, present=False)
 
     @steps_checker.step
     def get_role(self, *args, **kwgs):
-        """Step to find role."""
+        """Step to find role.
+
+        Args:
+            name (str or obj): the name of the role
+            domain (obj or obj): the domain of the role. If a value is passed
+                it is a domain-scoped role, otherwise it's a global role
+            kwgs could be: any other attribute provided will be passed to
+                the server
+
+        Returns:
+            object: role
+        """
         return self._client.find(*args, **kwgs)
 
     @steps_checker.step
-    def grant_role(self, role, user=None, group=None, domain=None,
-                   project=None, check=True):
-        """Step to grant role to user or group on domain or project."""
-        self._client.grant(role, user=user, group=group, domain=domain,
+    def grant_role(self,
+                   role,
+                   user=None,
+                   group=None,
+                   domain=None,
+                   project=None,
+                   check=True):
+        """Step to grant role to user or group on domain or project.
+
+        Args:
+            role (str or obj): the role to be granted on the server
+            user (str or obj): the specified user to have the role granted on
+                a resource. Domain or project must be specified.
+                User and group are mutually exclusive.
+            group (str or obj): the specified group to have the role granted
+                on a resource. Domain or project must be specified.
+                User and group are mutually exclusive.
+            domain (str or obj): the domain in which the role will be granted.
+                Either user or group must be specified. Project and domain
+                are mutually exclusive.
+            project (str or obj): the project in which the role will be
+                granted. Either user or group must be specified.
+                Project and domain are mutually exclusive.
+            check (bool): flag whether to check step or not
+
+        Raises:
+            NotFound: if check was triggered to an error
+        """
+        self._client.grant(role,
+                           user=user,
+                           group=group,
+                           domain=domain,
                            project=project)
         if check:
-            self.check_role_grant_status(role, user=user, group=group,
-                                         domain=domain, project=project)
+            self.check_role_grant_status(role,
+                                         user=user,
+                                         group=group,
+                                         domain=domain,
+                                         project=project)
 
     @steps_checker.step
-    def revoke_role(self, role, user=None, group=None, domain=None,
-                    project=None, check=True):
-        """Step to revoke role from user or group on domain or project."""
-        self._client.revoke(role, user=user, group=group, domain=domain,
+    def revoke_role(self,
+                    role,
+                    user=None,
+                    group=None,
+                    domain=None,
+                    project=None,
+                    check=True):
+        """Step to revoke role from user or group on domain or project.
+
+        Args:
+            role (str or obj): the role to be revoked on the server
+            user (str or obj): the specified user to have the role revoked on
+                a resource. Domain or project must be specified.
+                User and group are mutually exclusive.
+            group (str or obj): the specified group to have the role revoked
+                on a resource. Domain or project must be specified.
+                User and group are mutually exclusive.
+            domain (str or obj): the domain in which the role will be revoked.
+                Either user or group must be specified. Project and domain
+                are mutually exclusive.
+            project (str or obj): the project in which the role will be
+                revoked. Either user or group must be specified.
+                Project and domain are mutually exclusive.
+            check (bool): flag whether to check step or not
+
+        Raises:
+            NotFound: if check was triggered to an error
+        """
+        self._client.revoke(role,
+                            user=user,
+                            group=group,
+                            domain=domain,
                             project=project)
         if check:
-            self.check_role_grant_status(role, user=user, project=project,
-                                         group=group, domain=domain,
+            self.check_role_grant_status(role,
+                                         user=user,
+                                         project=project,
+                                         group=group,
+                                         domain=domain,
                                          granted=False)
 
     @steps_checker.step
-    def check_role_presence(self, role, present=True, timeout=0):
-        """Check step that role is present."""
-        def predicate():
+    def check_role_presence(self, role, must_present=True, timeout=0):
+        """Check step that role is present.
+
+        Args:
+            role (str or obj): the role to be checked on the server
+            must_present (bool): flag whether role should present or no
+            timeout (int): seconds to wait a result of check
+
+        Raises:
+            TimeoutExpired: if check is triggered to an error after timeout
+        """
+        def _check_role_presence():
             try:
                 self._client.get(role.id)
-                return present
-            except Exception:
-                return not present
+                is_present = True
+            except exceptions.NotFound:
+                is_present = False
 
-        wait(predicate, timeout_seconds=timeout)
+            return expect_that(is_present, equal_to(must_present))
+
+        waiter.wait(_check_role_presence, timeout_seconds=timeout)
 
     @steps_checker.step
-    def check_role_grant_status(self, role, user=None, group=None, domain=None,
-                                project=None, granted=True, timeout=0):
-        """Check step if a user or group has a role on a domain or project."""
-        def predicate():
-            try:
-                self._client.check(role, user=user, group=group, domain=domain,
-                                   project=project)
-                return granted
-            except Exception:
-                return not granted
+    def check_role_grant_status(self,
+                                role,
+                                user=None,
+                                group=None,
+                                domain=None,
+                                project=None,
+                                must_granted=True,
+                                timeout=0):
+        """Check step if a user or group has a role on a domain or project.
 
-        wait(predicate, timeout_seconds=timeout)
+        Args:
+            role (str or obj): the role to be checked on the server
+            user (str or obj): the specified user to have the role checked on
+                a resource. Domain or project must be specified.
+                User and group are mutually exclusive.
+            group (str or obj): the specified group to have the role checked
+                on a resource. Domain or project must be specified.
+                User and group are mutually exclusive.
+            domain (str or obj): the domain in which the role will be checked.
+                Either user or group must be specified. Project and domain
+                are mutually exclusive.
+            project (str or obj): the project in which the role will be
+                checked. Either user or group must be specified.
+                Project and domain are mutually exclusive.
+            must_granted (bool): flag whether role should present or no
+            timeout (int): seconds to wait a result of check
+
+        Raises:
+            TimeoutExpired: if check is triggered to an error after timeout
+        """
+        def _check_role_grant_status():
+            try:
+                self._client.check(role,
+                                   user=user,
+                                   group=group,
+                                   domain=domain,
+                                   project=project)
+                is_granted = True
+            except exceptions.NotFound:
+                is_granted = False
+
+            return expect_that(is_granted, equal_to(must_granted))
+
+        waiter.wait(_check_role_grant_status, timeout_seconds=timeout)
