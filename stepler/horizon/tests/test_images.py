3ebfbea65bb2e908491feabb7f3cb42a07b8517d
Alexandra Allakhverdieva <aallakhverdieva@mirantis.com>
2016-11-07 11:13:06 +0000

    [WIP] Add UI test check image privacy
    Change-Id: I081b7d7b07ef59c3cb7cf065d640d4144bb3a7a4

--- stepler/horizon/tests/test_images.py
+++ stepler/horizon/tests/test_images.py
@@ -118,3 +118,34 @@ class TestAnyOne(object):
                                      network_name=config.INTERNAL_NETWORK_NAME)
         instances_steps.check_instance_active(instance_name)
         instances_steps.delete_instance(instance_name)
+
+
+@pytest.mark.usefixtures('admin_only')
+class TestAdminOnly(object):
+    """Tests for admin only."""
+
+    @pytest.mark.idempotent_id('451ea18f-e513-48ac-999a-4e719516478e')
+    def test_image_privacy(self, image, images_steps, auth_steps):
+        """Verify that non public image is not visible for other users.
+
+        **Setup:**
+
+        # Create image with public=False
+
+
+        **Steps:**
+
+        #. Logout
+        #. Login as non-admin user to another project
+        #. Check that image is not available as public image
+
+        **Teardown:**
+
+        #. Delete image
+        """
+
+        auth_steps.logout()
+        auth_steps.login(config.USER_NAME, config.USER_PASSWD)
+        images_steps.check_non_public_image_not_visible(image.name)
+        auth_steps.logout()
+        auth_steps.login(config.ADMIN_NAME, config.ADMIN_PASSWD)
