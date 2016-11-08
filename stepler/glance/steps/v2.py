c912c92dfd70b9b7ac7dd34669ff0a9653fe2ba5
Artem <aminasyan@mirantis.com>
2016-11-08 13:58:50 +0300

    Add CLI test for Validate Glance image with API V2
    Change-Id: Ida10c3750c0692703b2e4fcd6c14e7fc24fd53d7

--- stepler/glance/steps/v2.py
+++ stepler/glance/steps/v2.py
@@ -19,6 +19,8 @@ Glance steps v2
 
 from hamcrest import assert_that, empty, is_not, equal_to, is_in  # noqa
 from glanceclient import exc
+from stepler.third_party.utils import get_md5sum
+
 
 from stepler import config
 from stepler.third_party.matchers import expect_that
@@ -38,7 +40,7 @@ class GlanceStepsV2(BaseGlanceSteps):
 
     @steps_checker.step
     def create_images(self,
-                      image_path,
+                      image_object,
                       image_names=None,
                       disk_format='qcow2',
                       container_format='bare',
@@ -49,7 +51,7 @@ class GlanceStepsV2(BaseGlanceSteps):
         """Step to create images.
 
         Args:
-            image_path (str): path to image at local machine
+            image_object (str or file): path to image at local machine or file
             image_names (list): names of created images, if not specified
                 one image name will be generated
             disk_format (str): format of image disk
@@ -78,22 +80,17 @@ class GlanceStepsV2(BaseGlanceSteps):
                 visibility=visibility,
                 **kwargs)
 
-            if upload:
-                self._client.images.upload(image.id, open(image_path, 'rb'))
+            if hasattr(image_object, 'read'):
+                self._client.images.upload(image.id, image_object)
+            else:
+                self._client.images.upload(image.id, open(image_object, 'rb'))
             images.append(image)
-
         if check:
             for image in images:
-                if upload:
-                    self.check_image_status(
-                        image,
-                        config.STATUS_ACTIVE,
-                        timeout=config.IMAGE_AVAILABLE_TIMEOUT)
-                else:
-                    self.check_image_status(
-                        image,
-                        config.STATUS_QUEUED,
-                        timeout=config.IMAGE_QUEUED_TIMEOUT)
+                self.check_image_status(
+                    image,
+                    config.STATUS_ACTIVE,
+                    timeout=config.IMAGE_AVAILABLE_TIMEOUT)
 
         return images
 
@@ -301,3 +298,18 @@ class GlanceStepsV2(BaseGlanceSteps):
         """
         image_id_changed = self.get_image(name=image_name)['id']
         assert_that(image_id_changed, is_not(image_id))
+
+    @steps_checker.step
+    def check_image_hash(self, image_1, image_2):
+        """Step to comparing checksum files.
+
+        Args:
+            obj1: path or file for comparing
+            obj2: path or file for comparing
+
+        Raises:
+            AssertionError: if check failed
+        """
+        md5_sum_first = get_md5sum(image_1)
+        md5_sum_second = get_md5sum(image_2)
+        assert_that(md5_sum_first, equal_to(md5_sum_second))
