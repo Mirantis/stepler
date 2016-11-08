9f5f112fa9ef167b145238a122eb2f087cadd6a9
Artem <aminasyan@mirantis.com>
2016-11-08 13:58:50 +0300

    Add CLI test for Validate Glance image  with API V2
    Change-Id: Ida10c3750c0692703b2e4fcd6c14e7fc24fd53d7

---
+++ stepler/cli_clients/fixtures/glance.py
@@ -0,0 +1,39 @@
+"""
+------------------------
+Glance CLI client fixtures
+------------------------
+"""
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+
+from stepler.cli_clients import steps
+
+__all__ = [
+    'cli_glance_steps',
+]
+
+
+@pytest.fixture
+def cli_glance_steps(remote_executor):
+    """Function fixture to glance CLI steps.
+
+    Args:
+        remote_executor (callable): function to execute command on remote node
+
+    Returns:
+        CliGlanceSteps: instantiated glance CLI steps.
+    """
+    return steps.CliGlanceSteps(remote_executor)
