21dcd7d83cdc2ed4a77930d967bafe595ab156cb
Sergey Sokolov <ssm@mera.ru>
2016-11-11 16:24:26 +0300

    Add Glance CLI tests for download zero-size image
    Change-Id: I0774c7553188168ee41715a6174d54287f8fe076

--- stepler/cli_clients/steps/glance.py
+++ stepler/cli_clients/steps/glance.py
@@ -20,6 +20,7 @@ Glance CLI client steps
 from hamcrest import assert_that, contains_string, equal_to  # noqa
 from six import moves
 
+from stepler import config
 from stepler.cli_clients.steps import base
 from stepler.third_party import output_parser
 from stepler.third_party import steps_checker
@@ -29,16 +30,16 @@ class CliGlanceSteps(base.BaseCliSteps):
     """CLI glance client steps."""
 
     @steps_checker.step
-    def create_image(self, image_file, disk_format, container_format,
-                     image_name=None, api_version=2, check=True):
+    def create_image(self, image_file=None, image_name=None, disk_format=None,
+                     container_format=None, api_version=2, check=True):
         """Step to create image.
 
         Args:
-            image_file (str): image file to be uploaded
-            image_name (str): name of created image
-            disk_format (str): disk format of image
-            container_format (str): container format of image
-            api_version (int): API version of Glance
+            image_file (str|None): image file to be uploaded
+            image_name (str|None): name of created image
+            disk_format (str|None): disk format of image
+            container_format (str|None): container format of image
+            api_version (int): API version of Glance (1 or 2)
             check (bool): flag whether to check result or not
 
         Returns:
@@ -57,6 +58,11 @@ class CliGlanceSteps(base.BaseCliSteps):
             cmd += ' --disk-format ' + disk_format
         if container_format:
             cmd += ' --container-format ' + container_format
+        if 'disk-format' not in cmd and 'container-format' not in cmd:
+            cmd += ' <&-'
+            # otherwise stderr: <stdin: is not a tty\nerror: Must provide
+            # --container-format, --disk-format when using stdin>
+            # This problem is only for remote execution (Ansible)
 
         exit_code, stdout, stderr = self.execute_command(
             cmd, environ={'OS_IMAGE_API_VERSION': api_version},
@@ -89,5 +95,36 @@ class CliGlanceSteps(base.BaseCliSteps):
             container_format=None,
             api_version=api_version,
             check=False)
+        assert_that(exit_code, equal_to(1))
+        assert_that(stderr, contains_string(error_message))
+
+    @steps_checker.step
+    def check_negative_download_zero_size_image(self, image_id,
+                                                progress=False,
+                                                api_version=2):
+        """Step to check that zero-size image cannot be downloaded.
+
+        Args:
+            image_id (str): image ID
+            progress (bool): option of download command
+            api_version (int): glance api version (1 or 2). Default is 2
+
+        Raises:
+            AssertionError: if command exit code is not 1 or stderr doesn't
+                contains expected message.
+        """
+        cmd = "glance image-download {}".format(image_id)
+        if progress:
+            cmd += " --progress"
+        if api_version == 1:
+            error_message = ("Image {} is not active (HTTP 404)".
+                             format(image_id))
+        else:
+            error_message = "Image {} has no data".format(image_id)
+
+        exit_code, stdout, stderr = self.execute_command(
+            cmd, environ={'OS_IMAGE_API_VERSION': int(api_version)},
+            timeout=config.IMAGE_CREATION_TIMEOUT, check=False)
+
         assert_that(exit_code, equal_to(1))
         assert_that(stderr, contains_string(error_message))
