c9245d8c8a9f1ab9d5229ba8f1653a9ffb3f85e2
Sergey Turivniy <turivniy@gmail.com>
2016-11-16 13:41:27 +0200

    Add test for baremetal chassis
    Change-Id: Iffe2a21e9432e9353e4485168535fc8e2c467042

---
+++ stepler/baremetal/tests/test_baremetal_chassis.py
@@ -0,0 +1,31 @@
+"""
+------------------------------
+Ironic baremetal chassis tests
+------------------------------
+"""
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import pytest
+
+
+@pytest.mark.idempotent_id('cde24671-65b2-46f5-b8e5-e3ff087e4da6')
+def test_chassis_create(ironic_chassis_steps):
+    """**Scenario:** Verify that ironic chassis can be created and deleted.
+
+    **Steps:**
+
+    #. Create ironic chassis
+    #. Delete ironic chassis
+    """
+    ironic_chassis_steps.create_ironic_chassis()
