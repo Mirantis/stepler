2c1344a1224077cebfb3f6a53101b445316d1372
Sergey Turivniy <turivniy@gmail.com>
2016-11-22 15:39:31 +0200

    Add API tests for baremetal nodes
    Change-Id: I838b0fe897574f8ba7b334b74191fcef71fd0f07

---
+++ stepler/baremetal/api_clients/node_v1.py
@@ -0,0 +1,42 @@
+"""
+----------------------
+Ironic node API client
+----------------------
+"""
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from stepler.base import Resource
+
+from .base import NodeApiClient
+
+
+class NodeApiClientV1(NodeApiClient):
+    """Ironic node API client."""
+
+    def node_list(self):
+        """Get nodes list via API call."""
+        response = self._get('/v1/nodes/')
+        response.raise_for_status()
+
+        nodes = [Resource(self, node_dict)
+                 for node_dict in response.json()['nodes']]
+        return nodes
+
+    def node_get(self, node_ident):
+        """Get node via API call."""
+        response = self._get('/v1/nodes/{}'.format(node_ident))
+        response.raise_for_status()
+
+        return Resource(self, response.json())
