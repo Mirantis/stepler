9689d9b2ee5da2b5c0319d72e121abbc4e6c812d
Georgy Dyuldin <g.dyuldin@gmail.com>
2016-11-24 11:58:23 +0300

    Improve skips logic.
    This commit adds display of actualy calculated condition. Also it
    calculates properties of Predicates only when them requires.
    
    Change-Id: Ib8c020605c821ba94cf7420ff34640e069472d07

--- stepler/fixtures/skip.py
+++ stepler/fixtures/skip.py
@@ -17,6 +17,9 @@ Skip autouse fixture
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import ast
+import functools
+
 import pytest
 
 from stepler import config
@@ -24,6 +27,9 @@ from stepler import config
 __all__ = [
     'skip_test',
 ]
+
+
+PREDICATES = 'predicates'
 
 
 @pytest.fixture(autouse=True)
@@ -34,8 +40,8 @@ def skip_test(request):
         .. code:: python
 
            @pytest.mark.requires("ceph_enabled")
-           @pytest.mark.requires("more_computes_than(4)")
-           @pytest.mark.requires("more_computes_than(2) and ceph_enabled")
+           @pytest.mark.requires("computes_count > 4")
+           @pytest.mark.requires("computes_count > 4 and ceph_enabled")
 
         It supports any logical and arithmetical operations and their
         combinations.
@@ -54,12 +60,29 @@ def skip_test(request):
         requires = ' and '.join(requires)
 
     predicates = Predicates(request)
-    predicates = {attr: getattr(predicates, attr) for attr in dir(predicates)
-                  if not attr.startswith('_')}
+    tree = ast.parse(requires, mode='eval')
 
-    if not eval(requires, predicates):
-        pytest.skip(
-            'Skipped due to a mismatch to condition: {!r}'.format(requires))
+    tree = RewritePredicates().visit(tree)
+    ast.fix_missing_locations(tree)
+    code = compile(tree, '<ast>', mode='eval')
+
+    if not eval(code, {PREDICATES: predicates}):
+        pytest.skip('Skipped due to a mismatch to condition: {!r}\n'
+                    'Calculated conditions: {}'.format(
+                        requires, predicates.get_calculated_conditions()))
+
+
+class RewritePredicates(ast.NodeTransformer):
+    """Class to rewrite requires to predicate instance attributes."""
+
+    def visit_Name(self, node):
+        return ast.copy_location(
+            ast.Attribute(
+                value=ast.Name(
+                    id=PREDICATES, ctx=ast.Load()),
+                attr=node.id,
+                ctx=node.ctx),
+            node)
 
 
 class Predicates(object):
@@ -68,6 +91,25 @@ class Predicates(object):
     def __init__(self, request):
         """Initialize."""
         self._request = request
+        self._calls = []
+
+    def store_call(f):
+        """Decorator to store each method call with result."""
+        @functools.wraps(f)
+        def wrapper(*args, **kwargs):
+            self = args[0]
+            result = f(*args, **kwargs)
+            self._calls.append([f.__name__, result])
+            return result
+        return wrapper
+
+    def get_calculated_conditions(self):
+        """Get all calculated conditions (names and results).
+
+        Returns:
+            str: string with all calculated conditions
+        """
+        return ', '.join(['{}={}'.format(*call) for call in self._calls])
 
     def _get_fixture(self, fixture_name):
         return self._request.getfixturevalue(fixture_name)
@@ -76,21 +118,25 @@ class Predicates(object):
         os_faults_steps = self._get_fixture('os_faults_steps')
         return os_faults_steps.get_network_type()
 
+    @store_call
     def computes_count_gte(self, count):
         """Define whether computes enough."""
         hypervisor_steps = self._get_fixture('hypervisor_steps')
         return len(hypervisor_steps.get_hypervisors()) >= count
 
     @property
+    @store_call
     def ceph_enabled(self):
         """Define whether CEPH enabled."""
 
     @property
+    @store_call
     def vlan(self):
         """Define whether neutron configures with vlan."""
         return self._network_type == config.NETWORK_TYPE_VLAN
 
     @property
+    @store_call
     def vxlan(self):
         """Define whether neutron configures with vxlan."""
         return self._network_type == config.NETWORK_TYPE_VXLAN
