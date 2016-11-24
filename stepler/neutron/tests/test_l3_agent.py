6f37e2f59b8af67b84faddbad95e070378ca391b
Alexander Gromov <agromov@mirantis.com>
2016-11-24 19:25:27 +0300

    Added 2 tests on banning all l3 agents and restart
    Added 2 tests on banning all l3 agents and then:
    1) restarting the first banned agent
    2) restarting the last banned agent
    Updated markers for tests on l3 agent.
    Added step to check pings from servers using ping plan.
    
    Change-Id: I5dfc89f289026fc8607b3566d3f76103ccd0cb97

--- stepler/neutron/tests/test_l3_agent.py
+++ stepler/neutron/tests/test_l3_agent.py
@@ -21,11 +21,12 @@ import pytest
 
 from stepler import config
 
-# TODO(agromov): add destructive marker after os-faults release
-pytestmark = pytest.mark.requires("l3_agent_nodes_count >= 3",
-                                  "computes_count >= 2")
+
+pytestmark = [pytest.mark.requires("computes_count >= 2"),
+              pytest.mark.destructive]
 
 
+@pytest.mark.requires("l3_agent_nodes_count >= 3")
 @pytest.mark.idempotent_id('e97bbed1-f1b2-4732-80f4-9b2919e846b2',
                            ban_count=1)
 @pytest.mark.idempotent_id('aa00657f-765b-410d-a614-3a699622f818',
@@ -62,7 +63,7 @@ def test_ban_one_l3_agent(cirros_image,
     #. Get node with l3 agent for router_1
     #. Ban l3 agent for the node with pcs
     #. Wait for l3 agent becoming dead
-    #. Check that router1 was rescheduled
+    #. Check that router_1 was rescheduled
     #. Repeat last 4 steps if ban_count is 2
     #. Boot server_3 in network_1
     #. Associate floating ip for server_3
@@ -109,18 +110,157 @@ def test_ban_one_l3_agent(cirros_image,
     server_steps.attach_floating_ip(server_3, floating_ip)
 
     server_1, server_2 = neutron_2_servers_diff_nets_with_floating.servers
-    server_steps.check_ping_between_servers_via_floating(
-        (server_1, server_3,),
-        ip_types=(config.FIXED_IP,),
-        timeout=config.PING_BETWEEN_SERVERS_TIMEOUT)
-    server_steps.check_ping_between_servers_via_floating(
-        (server_1, server_2,),
-        ip_types=(config.FLOATING_IP,),
-        timeout=config.PING_BETWEEN_SERVERS_TIMEOUT)
-    server_steps.check_ping_between_servers_via_floating(
-        (server_2, server_3,),
-        ip_types=(config.FLOATING_IP,),
-        timeout=config.PING_BETWEEN_SERVERS_TIMEOUT)
+    ping_plan = {
+        server_1: [(server_2, config.FLOATING_IP), (server_3,
+                                                    config.FIXED_IP)],
+        server_2: [(server_1, config.FLOATING_IP),
+                   (server_3, config.FLOATING_IP)],
+        server_3: [(server_2, config.FLOATING_IP), (server_1, config.FIXED_IP)]
+    }
+    server_steps.check_ping_by_plan(
+        ping_plan, timeout=config.PING_BETWEEN_SERVERS_TIMEOUT)
+
+
+@pytest.mark.requires("l3_agent_nodes_count >= 2")
+@pytest.mark.idempotent_id('35c373ee-6aa5-4a44-8d89-4a627b8351ce',
+                           agent_number=0)
+@pytest.mark.idempotent_id('14369db2-b36c-47e8-8c52-0deac890a268',
+                           agent_number=-1)
+@pytest.mark.parametrize('agent_number',
+                         [0, -1],
+                         ids=['first', 'last'])
+@pytest.mark.parametrize('neutron_2_networks',
+                         ['different_routers'],
+                         indirect=True)
+def test_ban_all_l3_agents_restart_one(
+        cirros_image,
+        flavor,
+        security_group,
+        neutron_2_servers_diff_nets_with_floating,
+        nova_create_floating_ip,
+        server_steps,
+        os_faults_steps,
+        agent_steps,
+        agent_number):
+    """**Scenario:** Ban all l3-agent agents and restart one.
+
+    **Setup:**
+
+    #. Create cirros image
+    #. Create flavor
+    #. Create security group
+    #. Create network_1 with subnet_1 and router_1
+    #. Create server_1
+    #. Create network_2 with subnet_2
+    #. Create server_2 on another compute and connect it to network_2
+    #. Assign floating ips to servers
+
+    **Steps:**
+
+    #. Check that ping from server_1 to server_2 with floating ips
+        and 8.8.8.8 is successful
+    #. Check that there is no ping between server_1 and server_2 via
+        internal ips.
+    #. Get node with l3 agent for router_1
+    #. Ban l3 agent for the node with pcs
+    #. Wait for l3 agent becoming dead
+    #. Check that router_1 was rescheduled
+    #. Repeat last 3 steps for all l3 agents except for one
+    #. Ban the remaining l3 agent
+    #. Wait for l3 agent becoming dead if agent_number is not 0
+    #. Clear the first/last banned l3 agent
+    #. Wait for l3 agent becoming active
+    #. Boot server_3 in network_1
+    #. Associate floating ip for server_3
+    #. Ping server_1 and server_3 from each other with both ips
+    #. Ping server_2 and server_3 from each other with floating ip
+    #. Check that ping from server_1 to 8.8.8.8 is successful
+    #. Check that ping from server_3 to 8.8.8.8 is successful
+
+    **Teardown:**
+
+    #. Delete servers
+    #. Delete networks, subnets, routers
+    #. Delete floating IPs
+    #. Delete security group
+    #. Delete cirros image
+    #. Delete flavor
+    """
+    network_1, network_2 = neutron_2_servers_diff_nets_with_floating.networks
+    server_1, server_2 = neutron_2_servers_diff_nets_with_floating.servers
+    router_1, _ = neutron_2_servers_diff_nets_with_floating.routers
+    server_2_fixed_ip = server_steps.get_fixed_ip(server_2)
+    server_2_floating_ip = server_steps.get_floating_ip(server_2,
+                                                        config.FLOATING_IP)
+
+    with server_steps.get_server_ssh(server_1) as server_ssh:
+        for ip_to_ping in (config.GOOGLE_DNS_IP, server_2_floating_ip):
+            server_steps.check_ping_for_ip(ip_to_ping,
+                                           server_ssh,
+                                           timeout=config.PING_CALL_TIMEOUT)
+        with server_steps.check_no_ping_context(
+                server_2_fixed_ip,
+                server_ssh=server_ssh):
+            pass
+
+    banned_agents = []
+    l3_agents_count = len(
+        agent_steps.get_agents(binary=config.NEUTRON_L3_SERVICE))
+
+    for number in range(l3_agents_count - 1):
+        l3_agent = agent_steps.get_l3_agents_for_router(router_1)[0]
+        nodes_with_l3 = os_faults_steps.get_nodes_for_l3_agents([l3_agent])
+        os_faults_steps.terminate_service(config.NEUTRON_L3_SERVICE,
+                                          nodes=nodes_with_l3)
+        agent_steps.check_alive([l3_agent],
+                                must_alive=False,
+                                timeout=config.NEUTRON_AGENT_ALIVE_TIMEOUT)
+        agent_steps.check_router_rescheduled(
+            router_1,
+            l3_agent,
+            timeout=config.L3_AGENT_RESCHEDULING_TIMEOUT)
+        banned_agents += [l3_agent]
+
+    l3_agent = agent_steps.get_l3_agents_for_router(router_1)[0]
+    nodes_with_l3 = os_faults_steps.get_nodes_for_l3_agents([l3_agent])
+    os_faults_steps.terminate_service(config.NEUTRON_L3_SERVICE,
+                                      nodes=nodes_with_l3)
+    # there are no active agents after we have banned the last
+    if agent_number != 0:
+        # we should check it only if we want to clear
+        # the last banned agent later
+        agent_steps.check_alive(
+            [l3_agent],
+            must_alive=False,
+            timeout=config.NEUTRON_AGENT_ALIVE_TIMEOUT)
+    banned_agents += [l3_agent]
+
+    agent_to_clear = banned_agents[agent_number]
+    nodes_with_l3 = os_faults_steps.get_nodes_for_l3_agents([agent_to_clear])
+    os_faults_steps.start_service(config.NEUTRON_L3_SERVICE,
+                                  nodes=nodes_with_l3)
+    agent_steps.check_alive([agent_to_clear],
+                            timeout=config.NEUTRON_AGENT_ALIVE_TIMEOUT)
+
+    server_3 = server_steps.create_servers(image=cirros_image,
+                                           flavor=flavor,
+                                           networks=[network_1],
+                                           security_groups=[security_group],
+                                           username=config.CIRROS_USERNAME,
+                                           password=config.CIRROS_PASSWORD)[0]
+    floating_ip = nova_create_floating_ip()
+    server_steps.attach_floating_ip(server_3, floating_ip)
+
+    ping_plan = {
+        server_1: [config.GOOGLE_DNS_IP, (server_2, config.FLOATING_IP),
+                   server_3],
+        server_2: [(server_1, config.FLOATING_IP),
+                   (server_3, config.FLOATING_IP)],
+        server_3: [config.GOOGLE_DNS_IP, (server_2, config.FLOATING_IP),
+                   server_1]
+    }
+    server_steps.check_ping_by_plan(
+        ping_plan, timeout=config.PING_BETWEEN_SERVERS_TIMEOUT)
 
 
 @pytest.mark.idempotent_id('e4338068-7a16-4db9-9645-68b074c91f91')
@@ -216,17 +356,13 @@ def test_ban_l3_agent_many_times(cirros_image,
     server_steps.attach_floating_ip(server_3, floating_ip)
 
     server_1, server_2 = neutron_2_servers_diff_nets_with_floating.servers
-
-    server_steps.check_ping_between_servers_via_floating(
-        (server_1, server_3,),
-        timeout=config.PING_BETWEEN_SERVERS_TIMEOUT)
-    server_steps.check_ping_between_servers_via_floating(
-        (server_2, server_3,),
-        ip_types=(config.FLOATING_IP,),
-        timeout=config.PING_BETWEEN_SERVERS_TIMEOUT)
-
-    for server in (server_1, server_3,):
-        with server_steps.get_server_ssh(server) as server_ssh:
-            server_steps.check_ping_for_ip(config.GOOGLE_DNS_IP,
-                                           server_ssh,
-                                           timeout=config.PING_CALL_TIMEOUT)
+    ping_plan = {
+        server_1: [config.GOOGLE_DNS_IP, (server_2, config.FLOATING_IP),
+                   server_3],
+        server_2: [(server_1, config.FLOATING_IP),
+                   (server_3, config.FLOATING_IP)],
+        server_3: [config.GOOGLE_DNS_IP, (server_2, config.FLOATING_IP),
+                   server_1]
+    }
+    server_steps.check_ping_by_plan(
+        ping_plan, timeout=config.PING_BETWEEN_SERVERS_TIMEOUT)
