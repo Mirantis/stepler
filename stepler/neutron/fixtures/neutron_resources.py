88f6408c55a4f92d537b254b4168b4dcc8214fd8
Georgy Dyuldin <g.dyuldin@gmail.com>
2016-11-28 16:11:23 +0300

    Add Neutron L3 HA "Disable/enable all L3 agents" test
    Change-Id: I98f215c2803060961fef530f0fd5428cf48cb3bb

--- stepler/neutron/fixtures/neutron_resources.py
+++ stepler/neutron/fixtures/neutron_resources.py
@@ -191,13 +191,17 @@ def neutron_2_servers_diff_nets_with_floating(
     Returns:
         attrdict.AttrDict: created resources
     """
-    servers = neutron_2_servers_different_networks.servers
+    resources = attrdict.AttrDict(neutron_2_servers_different_networks.copy())
 
-    for server in servers:
+    floating_ips = []
+    for server in resources.servers:
         floating_ip = nova_create_floating_ip()
         server_steps.attach_floating_ip(server, floating_ip)
+        floating_ips.append(floating_ip)
 
-    return neutron_2_servers_different_networks
+    resources.floating_ips = floating_ips
+
+    return resources
 
 
 @pytest.fixture
