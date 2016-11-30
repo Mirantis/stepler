2203207d33fb54f00dfcd77e3f8382c4a0998f14
Alexander Gromov <agromov@mirantis.com>
2016-11-30 15:28:44 +0300

    Added dhcp agent test on dropping port
    Change-Id: Idb799fe5fc91cc1069f22acda37250b8460594c7

--- stepler/neutron/steps/agents.py
+++ stepler/neutron/steps/agents.py
@@ -17,7 +17,7 @@ Agent steps
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from hamcrest import (assert_that, empty, is_in, is_not, only_contains,
+from hamcrest import (assert_that, empty, is_, is_in, is_not, only_contains,
                       has_entries)  # noqa H301
 
 from stepler import base
@@ -103,6 +103,25 @@ class AgentSteps(base.BaseSteps):
                     has_entries(**filter_attrs)))
 
         return dhcp_agents
+
+    @steps_checker.step
+    def check_agents_count_for_net(self, network, expected_count, timeout=0):
+        """Step to check DHCP agents count for network.
+
+        Args:
+            network (dict): network to check
+            expected_count (int): expected DHCP agents count for network
+            timeout (int): seconds to wait a result of check
+
+        Raises:
+            TimeoutExpired: if check failed after timeout
+        """
+        def _check_agents_count_for_net():
+            dhcp_agents = self.get_dhcp_agents_for_net(network)
+            return waiter.expect_that(len(dhcp_agents),
+                                      is_(expected_count))
+
+        waiter.wait(_check_agents_count_for_net, timeout_seconds=timeout)
 
     @steps_checker.step
     def check_network_rescheduled(self, network, old_dhcp_agent, timeout=0):
