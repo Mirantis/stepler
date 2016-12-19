231432fa5de1a45248107a87d7f95b07399063fa
Kyrylo Romanenko <kromanenko@mirantis.com>
2016-12-19 16:57:57 +0200

    Remove duplicated item from config
    Change-Id: Iebbf84c4fe888778b8b778ace2f372e192bec607

--- stepler/config.py
+++ stepler/config.py
@@ -145,7 +145,6 @@ STATUS_SUCCESS = 'success'
 STATUS_UPDATING = 'updating'
 STATUS_UPLOADING = 'uploading'
 STATUS_VERIFY_RESIZE = 'verify_resize'
-STATUS_ERROR = 'error'
 
 # TIMEOUTS (in seconds)
 # TODO(kromanenko): Investigate less intensive good polling interval value.
