d5495768b95afc5afa88989981a5ece776eae562
Alexander Gromov <agromov@mirantis.com>
2017-02-06 22:55:41 +0300

    Fix exception type for revert
    Change-Id: I2d897d319db0dae42ab1c958b1f6fe10115cc29b

--- stepler/third_party/destructive_dispatcher.py
+++ stepler/third_party/destructive_dispatcher.py
@@ -24,6 +24,7 @@ Destructive scenarios are marked via decorator ``@pytest.mark.destructive``.
 import logging
 import time
 
+from os_faults.ansible import executor
 import pytest
 
 from stepler import config
@@ -134,5 +135,5 @@ def revert_environment(destructor, snapshot_name):
     nodes.revert(snapshot_name)
     waiter.wait(lambda: nodes.run_task({'shell': 'hostname'}),
                 timeout_seconds=config.NODES_AVAILABILITY_TIMEOUT,
-                expected_exceptions=AssertionError)
+                expected_exceptions=executor.AnsibleExecutionException)
     nodes.run_task({'command': 'hwclock --hctosys'})
